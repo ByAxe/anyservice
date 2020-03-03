f325f5edb40cfb11f85e7ffa1383a7900cc5c8a2
byaxe <byaxe.sx@gmail.com>
2020-03-03 20:56:52 +0100

    Initial commit

---
+++ src/main/java/com/example/helloworld/AnyServiceController.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class AnyServiceController {
+
+    @Value("${TARGET:World}")
+    private String target;
+
+    @GetMapping
+    private String hello() {
+        return "Hello " + target + "!";
+    }
+}
